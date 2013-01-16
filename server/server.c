dfbdd67ed89e74ddb91f6628196320e3da469cd8
Moritz Dorka <moritz.dorka@mailbox.tu-dresden.de>
2013-01-16 15:12:31 +0100

    Improvements to Server (Structurize + Errorhandling)
    * split main() into different functions
    * enhance errorhandling
    * bind socket to single IP-Address

--- server/server.c
+++ server/server.c
@@ -2,33 +2,42 @@
 #include <stdlib.h> /* for exit() */
 #include <sys/socket.h> /* Socket Library */
 #include <netinet/in.h> /* IP-Communication */
-#include <stdio.h>
-#include <string.h> /* for memset() */
+#include <arpa/inet.h> /* conversion of IP-addresses */
+#include <stdio.h> /* I/O comunication */
+#include <string.h> /* memset() */
+#include <stdbool.h> /* boolean evaluation */
+#include <time.h> /* time functions */
+#include <errno.h> /* number of last error for error() */
 
 static const int PORTNUMBER=12345;
-//static const char CLIENTADDRESS=192.168.56.101;
+static const char CLIENTADDRESS[16]="127.0.0.1";
 static const int RECEIVE_DATA_LENGTH = 1;
 static const int SEND_DATA_LENGTH = 12;
 
-/* FIXME! ungarische Notation verwenden! */
-
 typedef int SOCKET;
+SOCKET sockethandler;
+struct sockaddr_in clientaddress;
+socklen_t clientaddresslength;
 
 void error(const char *errormessage)
 {
-  /* fatal */
-  perror(errormessage);
-  exit(1);
+  /* print errormessage including timestamp with millisecond precision */
+  struct timeval tp;
+  gettimeofday(&tp, 0);
+  time_t curtime = tp.tv_sec;
+  struct tm *t = localtime(&curtime);
+  char completemessage[80];
+
+  sprintf(completemessage, "%02d:%02d:%02d.%03d %s", t->tm_hour, t->tm_min, t->tm_sec, (int) tp.tv_usec/1000, errormessage);
+  perror(completemessage);
+  exit(-1);
 }
 
-int main()
+void open_socket()
 {
-  SOCKET sockethandler;
-  struct sockaddr_in ownaddress, clientaddress;
-  socklen_t clientaddresslength;
-  char receivedata[RECEIVE_DATA_LENGTH];
-  char senddata[SEND_DATA_LENGTH];
-  
+  /* creates a socket for upcoming communication */
+
+  struct sockaddr_in ownaddress;
 
   sockethandler = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); /*create an UDP socket*/
   if (sockethandler < 0 )
@@ -38,38 +47,71 @@ int main()
   
   memset( &ownaddress, 0, sizeof(ownaddress)); /* Initialize ownaddress with zeroes */
   ownaddress.sin_family = AF_INET;
-  //ownaddress.sin_addr.s_addr = htonl(CLIENTADDRESS); /* only accept connections from this IP */
-  ownaddress.sin_addr.s_addr = INADDR_ANY;
+  ownaddress.sin_addr.s_addr = htonl(inet_network(CLIENTADDRESS)); /* only accept connections from this IP, could be set to INADDR_ANY to accept all IPs */
   ownaddress.sin_port = htons(PORTNUMBER);
 
   if (bind(sockethandler, (struct sockaddr *) &ownaddress, sizeof(ownaddress)) < 0)
     {
-      error("Error during bind process");
+      error("Error during bind process.");
     }
-  
-  /* begin main server part */
-  clientaddresslength = sizeof(struct sockaddr_in);
-  while(1)
-    {
-      if (recvfrom(sockethandler, receivedata, RECEIVE_DATA_LENGTH, 0, (struct sockaddr *) &clientaddress, &clientaddresslength) < 0)
-	{
-	  error("Data could not be received.");
-	}
-      if ( *receivedata != '1' )
-	{
-	  fprintf(stderr, "No data was send due to wrong request code.\n");
-	  fprintf(stderr, "Request code was: %s, Should be: 1.\n", receivedata);
-	  continue;
-	}
-      /* answer the request */
-      //do something with senddata!
-      strcpy(senddata, "Hallo Welt!\n");
-      if (sendto(sockethandler, senddata, SEND_DATA_LENGTH, 0, (struct sockaddr *) &clientaddress, clientaddresslength) < 0)
-	{
-	  error("Error while sending data.");
-	}
-    }
-  close(sockethandler);
-  return 0;
 }
 
+void close_socket()
+{
+  close(sockethandler);
+}
+
+char receivefromclient()
+{
+  char receivedata[RECEIVE_DATA_LENGTH];
+  if (recvfrom(sockethandler, receivedata, RECEIVE_DATA_LENGTH, 0, (struct sockaddr *) &clientaddress, &clientaddresslength) < 0)
+    {
+      error("Data could not be received.");
+    }
+  return *receivedata;
+}
+
+bool evaluaterequest(const char *request, const char *comparevalue)
+{
+  /* checks if a proper request has been made by the client */
+  bool returncode = true;
+  if ( *request != *comparevalue )
+    {
+      fprintf(stderr, "No data was send due to wrong request code.\n");
+      fprintf(stderr, "Request code was: %i, Expected: %i\n", *request, *comparevalue); /*FIXME: use atoi?, does not work if we deal with strings */
+      /* FIXME: use error() in here somehow */
+      returncode = false;
+    }
+  return returncode;
+}
+
+void sendtoclient(const char *senddata)
+{
+  /* send data to a client */
+  if (sendto(sockethandler, senddata, SEND_DATA_LENGTH, 0, (struct sockaddr *) &clientaddress, clientaddresslength) < 0)
+    {
+      error("Error while sending data.");
+    }
+}
+
+
+int main()
+{
+  open_socket();
+
+  /* begin main server part */
+  clientaddresslength = sizeof(struct sockaddr_in);
+  while( true )
+    {
+      /* wait for request of data */
+      char requestresult;
+      requestresult = receivefromclient();
+      if ( evaluaterequest(&requestresult, "1") == false ) { continue; /* start all over and wait for next request */ }
+
+      /* answer the request */
+      sendtoclient("Hallo Welt!\n");
+    }
+
+  close_socket();
+  return 0;
+}
